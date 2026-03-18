781d9f30cc7900ec874539ed176ff9672fdfdb75
Meiryanne Martins <meirympereira@gmail.com>
2026-03-18 15:59:11 -0300

    feat: categories

--- src/data/categories.js
+++ src/data/categories.js
@@ -34,27 +34,9 @@ export const categories = [
     name: "Motor",
     icon: "https://cdn-icons-png.flaticon.com/512/2967/2967350.png",
     children: [
-      {
-        name: "Correias",
-        children: [
-          { name: "Dentada" },
-          { name: "Poly V" }
-        ]
-      },
-      {
-        name: "Juntas",
-        children: [
-          { name: "Cabeçote" },
-          { name: "Cárter" }
-        ]
-      },
-      {
-        name: "Pistões",
-        children: [
-          { name: "Gasolina" },
-          { name: "Diesel" }
-        ]
-      }
+      { name: "Correias", children: [{ name: "Dentada" }, { name: "Poly V" }] },
+      { name: "Juntas", children: [{ name: "Cabeçote" }, { name: "Cárter" }] },
+      { name: "Pistões", children: [{ name: "Gasolina" }, { name: "Diesel" }] }
     ]
   },
 
@@ -62,21 +44,8 @@ export const categories = [
     name: "Transmissão",
     icon: "https://cdn-icons-png.flaticon.com/512/1995/1995470.png",
     children: [
-      {
-        name: "Embreagem",
-        children: [
-          { name: "Kit Completo" },
-          { name: "Disco" },
-          { name: "Platô" }
-        ]
-      },
-      {
-        name: "Câmbio",
-        children: [
-          { name: "Manual" },
-          { name: "Automático" }
-        ]
-      }
+      { name: "Embreagem", children: [{ name: "Kit Completo" }, { name: "Disco" }, { name: "Platô" }] },
+      { name: "Câmbio", children: [{ name: "Manual" }, { name: "Automático" }] }
     ]
   },
 
@@ -84,20 +53,8 @@ export const categories = [
     name: "Suspensão",
     icon: "https://cdn-icons-png.flaticon.com/512/809/809957.png",
     children: [
-      {
-        name: "Amortecedores",
-        children: [
-          { name: "Dianteiro" },
-          { name: "Traseiro" }
-        ]
-      },
-      {
-        name: "Molas",
-        children: [
-          { name: "Helicoidal" },
-          { name: "Esportiva" }
-        ]
-      }
+      { name: "Amortecedores", children: [{ name: "Dianteiro" }, { name: "Traseiro" }] },
+      { name: "Molas", children: [{ name: "Helicoidal" }, { name: "Esportiva" }] }
     ]
   },
 
@@ -105,27 +62,9 @@ export const categories = [
     name: "Filtros",
     icon: "https://cdn-icons-png.flaticon.com/512/1046/1046857.png",
     children: [
-      {
-        name: "Filtro de Óleo",
-        children: [
-          { name: "Bosch" },
-          { name: "Mann" }
-        ]
-      },
-      {
-        name: "Filtro de Ar",
-        children: [
-          { name: "Esportivo" },
-          { name: "Original" }
-        ]
-      },
-      {
-        name: "Filtro de Combustível",
-        children: [
-          { name: "Diesel" },
-          { name: "Gasolina" }
-        ]
-      }
+      { name: "Filtro de Óleo", children: [{ name: "Bosch" }, { name: "Mann" }] },
+      { name: "Filtro de Ar", children: [{ name: "Esportivo" }, { name: "Original" }] },
+      { name: "Filtro de Combustível", children: [{ name: "Diesel" }, { name: "Gasolina" }] }
     ]
   },
 
@@ -133,20 +72,8 @@ export const categories = [
     name: "Lataria",
     icon: "https://cdn-icons-png.flaticon.com/512/743/743922.png",
     children: [
-      {
-        name: "Parachoque",
-        children: [
-          { name: "Dianteiro" },
-          { name: "Traseiro" }
-        ]
-      },
-      {
-        name: "Capô",
-        children: [
-          { name: "Original" },
-          { name: "Esportivo" }
-        ]
-      }
+      { name: "Parachoque", children: [{ name: "Dianteiro" }, { name: "Traseiro" }] },
+      { name: "Capô", children: [{ name: "Original" }, { name: "Esportivo" }] }
     ]
   },
 
@@ -154,20 +81,8 @@ export const categories = [
     name: "Freios",
     icon: "https://cdn-icons-png.flaticon.com/512/854/854878.png",
     children: [
-      {
-        name: "Pastilhas",
-        children: [
-          { name: "Dianteira" },
-          { name: "Traseira" }
-        ]
-      },
-      {
-        name: "Discos",
-        children: [
-          { name: "Ventilado" },
-          { name: "Sólido" }
-        ]
-      }
+      { name: "Pastilhas", children: [{ name: "Dianteira" }, { name: "Traseira" }] },
+      { name: "Discos", children: [{ name: "Ventilado" }, { name: "Sólido" }] }
     ]
   },
 
@@ -175,27 +90,9 @@ export const categories = [
     name: "Elétrica",
     icon: "https://cdn-icons-png.flaticon.com/512/3103/3103446.png",
     children: [
-      {
-        name: "Bateria",
-        children: [
-          { name: "60Ah" },
-          { name: "70Ah" }
-        ]
-      },
-      {
-        name: "Alternador",
-        children: [
-          { name: "Bosch" },
-          { name: "Valeo" }
-        ]
-      },
-      {
-        name: "Iluminação",
-        children: [
-          { name: "Farol" },
-          { name: "Lanterna" }
-        ]
-      }
+      { name: "Bateria", children: [{ name: "60Ah" }, { name: "70Ah" }] },
+      { name: "Alternador", children: [{ name: "Bosch" }, { name: "Valeo" }] },
+      { name: "Iluminação", children: [{ name: "Farol" }, { name: "Lanterna" }] }
     ]
   },
 
@@ -203,20 +100,8 @@ export const categories = [
     name: "Diversos",
     icon: "https://cdn-icons-png.flaticon.com/512/1828/1828919.png",
     children: [
-      {
-        name: "Acessórios",
-        children: [
-          { name: "Tapetes" },
-          { name: "Capas" }
-        ]
-      },
-      {
-        name: "Ferramentas",
-        children: [
-          { name: "Chaves" },
-          { name: "Macaco" }
-        ]
-      }
+      { name: "Acessórios", children: [{ name: "Tapetes" }, { name: "Capas" }] },
+      { name: "Ferramentas", children: [{ name: "Chaves" }, { name: "Macaco" }] }
     ]
   }
-]
+];
