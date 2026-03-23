a3214ef66119822b5db5bfc83b96c81cbd44af6e
Meiryanne Martins <meirympereira@gmail.com>
2026-03-23 12:23:21 -0300

    remove sub-categories

--- src/components/CategoriesMenu.jsx
+++ src/components/CategoriesMenu.jsx
@@ -23,33 +23,6 @@ export default function CategoriesMenu() {
             <img src={cat.icon} alt="" />
             <span>{cat.name}</span>
           </div>
-
-          {active === i && (
-            <div className="mega-menu">
-              {cat.children?.map((sub, j) => (
-                <div
-                  key={j}
-                  className="mega-column"
-                  onMouseEnter={() => setSubActive(j)}
-                >
-                  <div className="mega-item">
-                    {sub.name}
-                    {sub.children && <span className="arrow">▶</span>}
-                  </div>
-
-                  {subActive === j && sub.children && (
-                    <div className="sub-menu">
-                      {sub.children.map((item, k) => (
-                        <div key={k} className="sub-item">
-                          {item.name}
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              ))}
-            </div>
-          )}
         </li>
       ))}
     </ul>
