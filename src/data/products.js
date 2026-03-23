37fbddc243493b99b9dbb788191fe54d39129a29
Meiryanne Martins <meirympereira@gmail.com>
2026-03-23 13:46:45 -0300

    refactore: component products

---
+++ src/data/products.js
@@ -0,0 +1,14 @@
+export const products = [
+  {
+    id: 1,
+    name: "Kit Embreagem Hyundai New Tucson",
+    price: "5.310,00",
+    image: "https://images.tcdn.com.br/img/img_prod/946974/180_kit_embreagem_hyundai_new_tucson_1_6_16v_turbo_apos_2017_d7uf1_23955_2_082719328c45a2e0e466fd7bfae77db5.jpg"
+  },
+  {
+    id: 2,
+    name: "Bomba Injetora Volkswagen Man Tga Tgx Tgs 0445020208 / 0445020075",
+    price: "8.010,00",
+    image: "https://images.tcdn.com.br/img/img_prod/946974/180_bomba_injetora_volkswagen_man_tga_tgx_tgs_0445020208_0445020075_25125_4_5e6c8173717cefb4112629a2dc1d7ef8.jpg"
+  }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,7 @@ import Banner from "../components/Banner"
 import Footer from "../components/Footer"
 import ProductGrid from "../components/ProductGrid"
 import BenefitsBanner from "../components/BenefitsBanner"
-
-const products = Array.from({ length: 10 }).map((_, i) => ({
-  id: i,
-  name: "Produto Automotivo",
-  price: "199,90",
-  image: "https://via.placeholder.com/200"
-}))
+import { products } from "../data/products"
 
 export default function Home() {
   return (
