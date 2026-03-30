10fe72825caf80dd66757f92a5984de3af0b5010
Meiryanne Martins <meirympereira@gmail.com>
2026-03-30 09:43:21 -0300

    feat: mobile

---
+++ src/components/MegaMenu.jsx
@@ -0,0 +1,15 @@
+function MegaMenu() {
+  return (
+    <div className="mega-menu">
+
+      <div className="mega-column">
+        <div className="mega-item">Bicos Injetores</div>
+        <div className="mega-item">Bombas Injetoras</div>
+        <div className="mega-item">Válvulas IMV</div>
+        <div className="mega-item">Sensores de Pressão Rail</div>
+        <div className="mega-item">Conectores e Peças Auxiliares</div>
+      </div>
+
+    </div>
+  )
+}
